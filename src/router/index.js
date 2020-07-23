135911549e279df1840bb045153ee0af02425763
Hari Narayanan <smgdark@gmail.com>
2020-07-24 01:05:17 +0530

    basic vue with router configured

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Main from '../views/Main.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+	{
+		path: '/',
+		name: 'Home',
+		component: Main
+	},
+	{
+		path: '/404',
+		component: () => import(/* webpackChunkName: "404" */ '@/components/global/404.vue')
+	},
+	{
+		path: '*',
+		redirect: '/404'
+	}
+];
+
+const router = new VueRouter({
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes,
+});
+
+export default router;
---
+++ src/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header />
+    <Body />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Body from "./Body.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  name: "Main",
+  components: {
+    Header,
+    Footer,
+    Body,
+  },
+};
+</script>
