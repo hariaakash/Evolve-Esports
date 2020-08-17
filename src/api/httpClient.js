b081e3b7d0f5c5860dbf4f3c4837394ce816196b
Hari Narayanan <smgdark@gmail.com>
2020-08-17 17:06:50 +0530

    updated backend endpoint

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import VueCookies from 'vue-cookies';
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://evolveesports.xyz' : 'http://localhost:3000';
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://backend.evolveesports.xyz' : 'http://localhost:3000';
 
 const httpClient = axios.create({ baseURL, timeout: 5000 });
 
